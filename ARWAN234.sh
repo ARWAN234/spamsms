a37b3070e35918d7b316f1972eb895b9a7fbcb9d
ARWAN234 <59957317+ARWAN234@users.noreply.github.com>
2020-01-19 15:23:00 +0700

    Add files via upload

---
+++ ARWAN234.sh
@@ -0,0 +1,22 @@
+#!/system/bin/sh
+
+cowsay -f eyes ANONYMOUSE INDONESIA | lolcat
+toilet -f standard " WELCOME " -F gay
+echo -e $blue'======================================================'
+echo Login With...| lolcat
+echo -e $red'======================================================'
+echo Username-: arwanyusuf123@gmail.com | lolcat
+echo Password-: xxxxxxx | lolcat
+echo Group----: 4 Pakem Cyber Army | lolcat
+echo Forum----: Anonymous Indonesia | lolcat
+cd $home
+pkg update
+pkg upgrade
+pkg install php
+pkg install python2
+Pkg install git
+git clone https://github.com/ClayHackerTeam/Prankers
+ls
+cd prankers
+ls
+sh prankers.sh
